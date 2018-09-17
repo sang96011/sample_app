ba33097edc71456e7941d57e4fa84cf2a3a8673d
Van Sang Nguyen <vansang.nguyen96@gmail.com>
2018-09-17 20:50:18 +0700

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,12 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    @user = User.find_by email: params[:session][:email].downcase
+    if @user&.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == Settings.login.remember ?
+        remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash[:danger] = t "flash.err_signin"
       render :new
@@ -13,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
